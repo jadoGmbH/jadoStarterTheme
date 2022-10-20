51b203899285182740d9c3ec2885e52d71f0b7e6
Albert Augustin <albert@ja.do>
2022-10-20 14:51:01 +0200

    cpt

--- archive-custom_type.php
+++ archive-custom_type.php
@@ -5,11 +5,8 @@
         <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
             <article id="post-<?php the_ID(); ?>" <?php post_class('cf'); ?>>
                 <header class="article-header">
-                    <h3 class="h2"><a href="<?php the_permalink() ?>" rel="bookmark"
-                                      title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h3>
-                    <p class="byline"><?php
-                        printf(__('Posted <time class="updated" datetime="%1$s" >%2$s</time> by <span class="author">%3$s</span>', 'jadotheme'), get_the_time('Y-m-j'), get_the_time(__('F jS, Y', 'jadotheme')), get_author_posts_url(get_the_author_meta('ID')));
-                        ?></p>
+                    <h3 class="h2">
+                        <a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h3>
                 </header>
                 <section class="entry-content">
                     <?php the_excerpt(); ?>
@@ -19,11 +16,10 @@
         else : ?>
             <article id="post-not-found" class="hentry">
                 <header class="article-header">
-                    <h1>404 - Site not found!</h1>
+                    <h1><?php __('404 - Site not found!'); ?></h1>
                 </header>
             </article>
         <?php endif; ?>
-        <?php //get_sidebar(); ?>
     </div>
 </div>
 <?php get_footer(); ?>
--- single-custom_type.php
+++ single-custom_type.php
@@ -1,57 +1,27 @@
-<?php
-/*
- * CUSTOM POST TYPE TEMPLATE
- *
- * This is the custom post type post template. If you edit the post type name, you've got
- * to change the name of this template to reflect that name change.
- *
- * For Example, if your custom post type is "register_post_type( 'bookmarks')",
- * then your single template should be single-bookmarks.php
- *
- * Be aware that you should rename 'custom_cat' and 'custom_tag' to the appropiate custom
- * category and taxonomy slugs, or this template will not finish to load properly.
- *
- * For more info: http://codex.wordpress.org/Post_Type_Templates
-*/
-?>
-
 <?php get_header(); ?>
 <div id="content">
     <div id="inner-content" class="wrap">
+        <div class="entry-content">
             <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
                 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
                     <header class="article-header">
                         <h1 class="single-title custom-post-type-title"><?php the_title(); ?></h1>
-                        <p class="byline vcard"><?php
-                            printf(__('Posted <time class="updated" datetime="%1$s">%2$s</time> by <span class="author">%3$s</span> <span class="amp">&</span> filed under %4$s.', 'jadotheme'), get_the_time('Y-m-j'), get_the_time(get_option('date_format')), get_the_author_link(), get_the_term_list($post->ID, 'custom_cat', ' ', ', '));
-                            ?></p>
                     </header>
                     <section class="entry-content">
                         <?php
                         the_content();
-//                        wp_link_pages(array(
-//                            'before' => '<div class="page-links"><span class="page-links-title">' . __('Pages:', 'jadotheme') . '</span>',
-//                            'after' => '</div>',
-//                            'link_before' => '<span>',
-//                            'link_after' => '</span>',
-//                        ));
                         ?>
-                    </section> <!-- end article section -->
-                    <footer class="article-footer">
-                        <p class="tags"><?php echo get_the_term_list(get_the_ID(), 'custom_tag', '<span class="tags-title">' . __('Custom Tags:', 'jadotheme') . '</span> ', ', ') ?></p>
-                    </footer>
-                    <?php //comments_template(); ?>
+                    </section>
                 </article>
-            <?php endwhile; ?>
-
-            <?php else : ?>
+            <?php endwhile;
+            else : ?>
                 <article id="post-not-found" class="hentry">
                     <header class="article-header">
-                        <h1>404 - Site not found!</h1>
+                        <h1><?php __('404 - Site not found!'); ?></h1>
                     </header>
                 </article>
             <?php endif; ?>
-        <?php //get_sidebar(); ?>
+        </div>
     </div>
 </div>
 <?php get_footer(); ?>
