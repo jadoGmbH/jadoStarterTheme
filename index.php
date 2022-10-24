0e3e89211256ff0d1be6a0787900225078dfd3ba
Albert Augustin <albert@ja.do>
2022-10-24 10:44:42 +0200

    cpt und post img

--- index.php
+++ index.php
@@ -2,34 +2,44 @@
 <div id="content">
     <div id="inner-content" class="wrap">
         <div class="entry-content blog">
-            <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-                <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-                    <h2 class="h2 entry-title">
-                        <a href="<?php the_permalink() ?>" rel="bookmark"
-                           title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a>
-                    </h2>
-                    <p class="byline entry-meta vcard">
-                        <?php printf(__('', 'jadotheme') . ' %1$s %2$s',
-                            '<time class="updated entry-time" datetime="' . get_the_time('Y-m-d') . '" >' . get_the_time(get_option('date_format')) . '</time>',
-                            ''
-                        ); ?>
-                    </p>
-                    <section class="entry-content">
-                        <p>
-                            <?php echo get_the_excerpt();  ?>
-                        </p>
-                    </section>
-                    <a href="<?php the_permalink() ?>" rel="bookmark" class="wp-block-button__link"
-                       title="<?php the_title_attribute(); ?>"><?php _e('read more', 'jadotheme'); ?></a>
-                    <?php //printf('<p class="footer-category">' . __('filed under', 'jadotheme') . ': %1$s</p>', get_the_category_list(', ')); ?>
-                    <?php //the_tags('<p class="footer-tags tags"><span class="tags-title">' . __('Tags:', 'jadotheme') . '</span> ', ', ', '</p>'); ?>
-                </article>
-            <?php endwhile;
-            else : ?>
-                <article id="post-not-found" class="hentry">
-                    <h1><?php _e('404 - Site not found!', 'jadotheme'); ?></h1>
-                </article>
-            <?php endif; ?>
+            <div class="posts">
+                <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+                    <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+                        <a rel="bookmark" href="<?php echo get_the_permalink(); ?>">
+                            <?php if (has_post_thumbnail()) {
+                                echo '<div class="postimg">';
+                                the_post_thumbnail('medium');
+                                echo '</div>';
+                            } ?>
+                            <div class="postcontent <?php if (!has_post_thumbnail()) {
+                                echo 'noimg';
+                            } ?>">
+                                <h2 class="h2 entry-title">
+                                    <?php the_title(); ?>
+                                </h2>
+                                <p class="byline entry-meta">
+                                    <?php printf(__('', 'jadotheme') . ' %1$s %2$s',
+                                        '<time class="updated entry-time" datetime="' . get_the_time('Y-m-d') . '" >' . get_the_time(get_option('date_format')) . '</time>',
+                                        ''
+                                    ); ?>
+                                </p>
+                                <p>
+                                    <?php echo get_the_excerpt(); ?>
+                                </p>
+                            </div>
+                        </a>
+                        <div class="postcontent">
+                            <?php printf('<p class="post-category">' . __('Category:', 'jadotheme') . ' %1$s</p>', get_the_category_list(', ')); ?>
+                            <?php the_tags('<p class="post-tags"><span class="tags-title">' . __('Tags:', 'jadotheme') . '</span> ', ', ', '</p>'); ?>
+                        </div>
+                    </article>
+                <?php endwhile;
+                else : ?>
+                    <article id="post-not-found" class="hentry">
+                        <h1><?php _e('404 - Site not found!', 'jadotheme'); ?></h1>
+                    </article>
+                <?php endif; ?>
+            </div>
             <?php //get_sidebar(); ?>
         </div>
     </div>
--- page-custom-loop.php
+++ page-custom-loop.php
@@ -11,28 +11,33 @@ get_header(); ?>
                 <section class="entry-content">
                     <h1><?php the_title(); ?></h1>
                     <div class="cpt-container">
-                    <?php
-                    $loop = new WP_Query(array('post_type' => 'custom_type'));
-                    if ($loop->have_posts()) :
-                        while ($loop->have_posts()) : $loop->the_post();
-                            ?>
-                            <div class="cpt <?php if(has_term('sold', 'custom_cat')){ // custom categories
-                                echo 'sold';
-                            }?>">
-                                <a href="<?php echo get_the_permalink(); ?>">
-                                    <?php if(has_post_thumbnail()){
-                                        echo '<div class="cptimg">';
-                                        the_post_thumbnail('medium');
-                                        echo '</div>';
-                                    } ?>
-                                    <h2><?php echo get_the_title(); ?></h2>
-                                    <?php the_excerpt(); ?>
-                                </a>
-                            </div>
                         <?php
-                        endwhile;
-                    endif;
-                    wp_reset_postdata(); ?>
+                        $loop = new WP_Query(array('post_type' => 'custom_type'));
+                        if ($loop->have_posts()) :
+                            while ($loop->have_posts()) : $loop->the_post();
+                                ?>
+                                <div class="cpt <?php if (has_term('sold', 'custom_cat')) { // custom categories
+                                    echo 'sold';
+                                } ?>">
+                                    <a rel="bookmark" href="<?php echo get_the_permalink(); ?>">
+                                        <?php if (has_post_thumbnail()) {
+                                            echo '<div class="cptimg">';
+                                            the_post_thumbnail('medium');
+                                            echo '</div>';
+                                        } ?>
+                                        <div class="cptcontent <?php if (!has_post_thumbnail()) {
+                                            echo 'noimg';
+                                        } ?>">
+                                            <h2><?php echo get_the_title(); ?></h2>
+                                            <?php the_excerpt(); ?>
+                                        </div>
+                                    </a>
+
+                                </div>
+                            <?php
+                            endwhile;
+                        endif;
+                        wp_reset_postdata(); ?>
                     </div>
                     <?php the_content(); ?>
                 </section>
