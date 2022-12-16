54aa05a24cbb0e590ed092952ed970fde5d4af58
jado <jadoGmbH@users.noreply.github.com>
2022-12-16 09:42:22 +0100

    header

--- header.php
+++ header.php
@@ -2,7 +2,6 @@
 <html lang="<?php echo get_bloginfo('language'); ?>">
 <head>
     <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <?php
     $description = get_bloginfo("description");
     if ($description != '') {
