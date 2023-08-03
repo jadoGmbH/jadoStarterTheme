b47174d076e3eb1569149296b5d12c2a55f72572
Albert Augustin <albert@ja.do>
2023-08-03 16:57:57 +0200

    fix

--- page.php
+++ page.php
@@ -1,6 +1,17 @@
 <?php get_header(); ?>
 <div id="content">
-    <div id="inner-content" class="wrap">
+    <?php if (has_post_thumbnail()) {
+        echo '<div class="featuredImage">';
+        the_post_thumbnail('featuredImage');
+        echo '</div>';
+    } ?>
+    <div id="inner-content" class="wrap <?php if (has_post_thumbnail()) {
+        if (class_exists('WooCommerce')) {
+            if (!is_product()) {
+                echo 'hasThumb';
+            }
+        }
+    } ?>">
         <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
             <div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
                 <section class="entry-content">
